7365a48fe4fbc00239af6a9cc78743c029bf66a0
Muasa-harman <harman.muasa@gmail.com>
2023-11-29 23:03:24 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import SignIn from './pages/Signin'
 import Signup from './pages/Signup'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/profile' element={<Profile/>}/>
